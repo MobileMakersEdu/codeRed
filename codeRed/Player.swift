f3182526b9a92bd5a23d5d2df35b7dbadf29ff27
vikdenic <vik.denic@gmail.com>
2014-09-20 16:43:11 -0500

    Add Enums class with ColliderType enum

--- codeRed/Player.swift
+++ codeRed/Player.swift
@@ -9,13 +9,6 @@
 import SpriteKit
 
 class Player {
-
-    enum ColliderType: UInt32 {
-        case Player = 1
-        case Bug = 2
-        case GoodChallenge = 4
-        case ProTip = 8
-    }
    
     var livesLeft = kPlayerStartingLives
     var name : String? = nil
