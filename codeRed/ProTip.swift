c50f593e91a4cce873c59d4cce741979fd5b03c9
Dave Krawczyk <dave.krawczyk@gmail.com>
2014-09-20 15:06:40 -0500

    protip stuff

--- codeRed/ProTip.swift
+++ codeRed/ProTip.swift
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Mobile Makers. All rights reserved.
 //
 
-enum ProTipMaker{
+enum ProTipMaker : UInt32{
     case Dave
     case Rich
     case Vik
@@ -15,10 +15,45 @@ enum ProTipMaker{
     case Max
 }
 
+import SpriteKit
 import UIKit
 
 class ProTip: Obstacle {
     
-    var maker: ProTipMaker? = nil
+    var maker: ProTipMaker! = nil
+    
+    
+    init(maker: ProTipMaker)
+    {
+        self.maker = maker
+        let color = UIColor()
+        let texture = SKTexture(image: ProTip.imageForMaker(maker))
+        let size = texture.size()
+        super.init(texture: texture, color: color, size: size)
+    }
+    
+    
+    class func imageForMaker(maker: ProTipMaker) -> UIImage {
+        
+        var imageName : NSString! = "obstacle_"
+        switch maker
+        {
+            case .Dave:
+                imageName + "dave"
+            case .Don:
+                imageName + "don"
+            case .Max:
+                imageName + "max"
+            case .Kevin:
+                imageName + "kevin"
+            case .Vik:
+                imageName + "vik"
+            case .Rich:
+                imageName + "rich"
+        }
+        
+        return UIImage(named: imageName)
+        
+    }
    
 }
