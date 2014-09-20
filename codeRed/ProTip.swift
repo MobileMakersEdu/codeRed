6310bd4845b36a3df96a14a5b4c6e5d503d4f5b0
Dave Krawczyk <dave.krawczyk@gmail.com>
2014-09-20 13:52:52 -0500

    starting player

--- codeRed/ProTip.swift
+++ codeRed/ProTip.swift
@@ -6,8 +6,19 @@
 //  Copyright (c) 2014 Mobile Makers. All rights reserved.
 //
 
+enum ProTipMaker{
+    case Dave
+    case Rich
+    case Vik
+    case Don
+    case Kevin
+    case Max
+}
+
 import UIKit
 
 class ProTip: Obstacle {
+    
+    var maker: ProTipMaker? = nil
    
 }
